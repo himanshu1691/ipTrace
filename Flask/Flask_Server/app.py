3ae78969bb0f67b2d6b31c9b3f0c5fe0736b6b83
Himanshu Jain <Himanshu Jain>
2016-05-06 11:14:38 -0700

    updating flask code

--- Flask/Flask_Server/app.py
+++ Flask/Flask_Server/app.py
@@ -17,36 +17,38 @@ def main():
 	ipadd = request.args.get('ipadd')
 
 	try:
-		splitip =  ipadd.split('.');
-		genip = "";
+		try:
+			splitip =  ipadd.split('.');
+			genip = "";
 
-		for parts in range(3):
-			genip = genip+splitip[parts]
-			genip = genip+"."
+			for parts in range(3):
+				genip = genip+splitip[parts]
+				genip = genip+"."
 
-		genip = genip[:-1]
+			genip = genip[:-1]
 
-		result = r_server.get(genip)
-		return result
-	except:
-		print("Redis connection failed..Fetching from File")
-		IP2LocObj = IP2Location.IP2Location()
-		IP2LocObj.open("../IP-COUNTRY-REGION-CITY-LATITUDE-LONGITUDE-ZIPCODE-TIMEZONE-ISP-DOMAIN-NETSPEED-AREACODE-WEATHER-MOBILE-ELEVATION-USAGETYPE-SAMPLE.BIN");
-		ip_detail = IP2LocObj.get_all("98.5.10.1")
+			result = r_server.get(genip)
+			return result
+		except:
+			print("Redis connection failed..Fetching from File")
+			IP2LocObj = IP2Location.IP2Location()
+			IP2LocObj.open("../IP-COUNTRY-REGION-CITY-LATITUDE-LONGITUDE-ZIPCODE-TIMEZONE-ISP-DOMAIN-NETSPEED-AREACODE-WEATHER-MOBILE-ELEVATION-USAGETYPE-SAMPLE.BIN");
+			ip_detail = IP2LocObj.get_all("98.5.10.1")
 
-		data = {}
-		data['latitude'] = ip_detail.latitude
-		data['longitude'] = ip_detail.longitude
-		data['country'] = ip_detail.country_long
-		data['region'] = ip_detail.region
-		data['city'] = ip_detail.city
+			data = {}
+			data['latitude'] = ip_detail.latitude
+			data['longitude'] = ip_detail.longitude
+			data['country'] = ip_detail.country_long
+			data['region'] = ip_detail.region
+			data['city'] = ip_detail.city
 
-		ip_detail_json = json.dumps(data)
-
-		return ip_detail_json
+			ip_detail_json = json.dumps(data)
+			return ip_detail_json
+	except Exception as e:
+		print(str(e))
 
 	
 
 
 if __name__ == "__main__":
-    app.run()	
+    app.run(debug=True)	
