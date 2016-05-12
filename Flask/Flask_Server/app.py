3b03e70d6d8843994d97d17fb3f1755838ae09ad
Nagkumar <me@nagkumar.com>
2016-05-12 10:17:18 -0700

    Connecting to redis and using if/else instead of try/catch

--- Flask/Flask_Server/app.py
+++ Flask/Flask_Server/app.py
@@ -1,55 +1,54 @@
-from flask import Flask , request
+from flask import Flask, request
 import redis
-import simplejson as json 
+import simplejson as json
 
 import sys
+
 sys.path.insert(0, '../')
 
-import IP2Location;
-
+import IP2Location
 
 app = Flask(__name__)
 
+
 @app.route("/iptrace")
 def main():
+    # 0.38.152 test ip
+    ipadd = request.args.get('ipadd')
 
-	#0.38.152 test ip
-	ipadd = request.args.get('ipadd')
+    try:
+        splitip = ipadd.split('.')
+        genip = ""
 
-	try:
-		try:
-			splitip =  ipadd.split('.');
-			genip = "";
+        for parts in range(3):
+            genip = genip + splitip[parts]
+            genip += "."
 
-			for parts in range(3):
-				genip = genip+splitip[parts]
-				genip = genip+"."
+        genip = genip[:-1]
+        r_server = redis.Redis('localhost')
+        result = r_server.get(genip)
+        if result:
+            return result
+        else:
+            print("Redis connection failed..Fetching from File")
+            IP2LocObj = IP2Location.IP2Location()
+            IP2LocObj.open(
+                "../IP-COUNTRY-REGION-CITY-LATITUDE-LONGITUDE-ZIPCODE-TIMEZONE-ISP-DOMAIN-NETSPEED-AREACODE-WEATHER-MOBILE-ELEVATION-USAGETYPE-SAMPLE.BIN")
+            ip_detail = IP2LocObj.get_all(ipadd)
 
-			genip = genip[:-1]
+            data = {}
+            data['latitude'] = ip_detail.latitude
+            data['longitude'] = ip_detail.longitude
+            data['country'] = ip_detail.country_long
+            data['region'] = ip_detail.region
+            data['city'] = ip_detail.city
 
-			result = r_server.get(genip)
-			return result
-		except:
-			print("Redis connection failed..Fetching from File")
-			IP2LocObj = IP2Location.IP2Location()
-			IP2LocObj.open("../IP-COUNTRY-REGION-CITY-LATITUDE-LONGITUDE-ZIPCODE-TIMEZONE-ISP-DOMAIN-NETSPEED-AREACODE-WEATHER-MOBILE-ELEVATION-USAGETYPE-SAMPLE.BIN");
-			ip_detail = IP2LocObj.get_all(ipadd)
+            ip_detail_json = json.dumps(data)
+            print(ip_detail_json)
+            return ip_detail_json
 
-			data = {}
-			data['latitude'] = ip_detail.latitude
-			data['longitude'] = ip_detail.longitude
-			data['country'] = ip_detail.country_long
-			data['region'] = ip_detail.region
-			data['city'] = ip_detail.city
-
-			ip_detail_json = json.dumps(data)
-			print(ip_detail_json)
-			return ip_detail_json
-	except Exception as e:
-		print(str(e))
-
-	
-
+    except Exception as e:
+        print(str(e))
 
 if __name__ == "__main__":
-    app.run(debug=True)	
+    app.run(debug=True)
